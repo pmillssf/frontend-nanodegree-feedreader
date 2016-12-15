ebf74a0b2c740424c149f8873b5e07ef9af31bf8
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2016-12-14 17:31:25 -0800

    Add "the menu" test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -56,6 +56,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -68,6 +69,7 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
